522cf5f9ae10719e2544c0093a2d6fd2a41e3b2d
PierrC <PierrC@users.noreply.github.com>
2019-02-09 16:46:58 -0500

    added basic controls + beginning of dialogue

---
+++ FeelGoodFarm/FeelGoodFarm-Game/Assets/_Scripts/DialogueManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI DialogueText;
+
+    private Queue<string> sentences;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Starting conversation with " + dialogue.name);
+        nameText.text = dialogue.name;
+        sentences.Clear();
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        // DialogueText.text = sentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        DialogueText.text = "";
+        foreach(char c in sentence.ToCharArray())
+        {
+            DialogueText.text += c;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation");
+    }
+}
---
+++ FeelGoodFarm/FeelGoodFarm-Game/Assets/_Scripts/Pawn.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Pawn : MonoBehaviour
+{
+
+    [SerializeField]
+    protected string Name;
+
+    protected Rigidbody2D rigidbody;
+
+
+
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+    
+    public void Hub_Move(Vector2 v)
+    {
+        rigidbody.velocity = v;
+    }
+
+    public virtual void TriggerDialogue()
+    {
+        Debug.Log("Basic interacting");
+    }
+
+}
