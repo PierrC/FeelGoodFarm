13cc57a6420ebd386a8306cfeb81404e4388ab3f
Anuradha Mallik <anuradhamallik@N30014847.localdomain>
2019-02-09 23:53:50 -0500

    sound tingz

--- FeelGoodFarm/FeelGoodFarm-Game/Assets/SoundTrigger.cs
+++ FeelGoodFarm/FeelGoodFarm-Game/Assets/SoundTrigger.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class SoundTrigger : MonoBehaviour
 {
+    public AudioClip triggerBeachSound;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("collision tag:" + collision.tag);
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Correct tag");
+            audioSource.PlayOneShot(triggerBeachSound, 0.7f);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+
+        audioSource.Stop();
     }
 }
