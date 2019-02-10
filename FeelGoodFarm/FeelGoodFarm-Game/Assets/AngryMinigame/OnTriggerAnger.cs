0061ebe324ffa1d854f2baea2bd38e01014e0d0a
Anuradha Mallik <anuradhamallik@N30014847.localdomain>
2019-02-09 20:54:44 -0500

    Packaged piano minigame

---
+++ FeelGoodFarm/FeelGoodFarm-Game/Assets/AngryMinigame/OnTriggerAnger.cs
@@ -0,0 +1,31 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnTriggerAnger : MonoBehaviour
+{
+    public InputField inputField;
+
+    GameController controller;
+
+    void Awake()
+    {
+        controller = GetComponent<GameController>();
+        inputField.onEndEdit.AddListener(AcceptStringInputstring);
+    }
+
+    void AcceptStringInputstring (string userInput)
+    {
+        controller.LogStringWithReturn(userInput);
+        InputComplete;
+    }
+
+    void InputComplete()
+    {
+        controller.DisplayLoggedText();
+        inputField.ActivateInputField();
+        inputField.text = null;
+    }
+
+}*/
