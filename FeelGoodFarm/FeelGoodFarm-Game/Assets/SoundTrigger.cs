2e832c0b9f62351e9e520403a2a65b23a1dda8e9
Anuradha Mallik <anuradhamallik@N30014847.localdomain>
2019-02-10 00:09:24 -0500

    progress on sounds

--- FeelGoodFarm/FeelGoodFarm-Game/Assets/SoundTrigger.cs
+++ FeelGoodFarm/FeelGoodFarm-Game/Assets/SoundTrigger.cs
@@ -16,7 +16,7 @@ public class SoundTrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -25,13 +25,31 @@ public class SoundTrigger : MonoBehaviour
         if (collision.tag == "Player")
         {
             Debug.Log("Correct tag");
-            audioSource.PlayOneShot(triggerBeachSound, 0.7f);
+            audioSource.PlayOneShot(triggerBeachSound, 0.1f);
+            fadeIn();
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
 
-        audioSource.Stop();
+            audioSource.Stop();
+
+    }
+
+    public void fadeIn()
+    {
+        if (audioSource.volume < 1)
+        {
+            audioSource.volume = audioSource.volume + 0.1f * Time.deltaTime;
+        }
+    }
+
+    public void fadeOut()
+    {
+        if (audioSource.volume > 0)
+        {
+            audioSource.volume = audioSource.volume - 0.1f * Time.deltaTime;
+        }
     }
 }
